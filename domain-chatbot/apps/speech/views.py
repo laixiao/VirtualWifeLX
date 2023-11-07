fda5de33c86f9fb6c7fc49450beb889f24a0dcdf
642024149 <642024149@qq.com>
2023-11-07 17:57:23 +0800

    格式化

--- domain-chatbot/apps/speech/views.py
+++ domain-chatbot/apps/speech/views.py
@@ -13,23 +13,23 @@ from django.http import HttpResponse, StreamingHttpResponse
 
 logger = logging.getLogger(__name__)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def generate(request):
     """
     Generate audio from text.
     """
     try:
-        data = json.loads(request.body.decode('utf-8'))
+        data = json.loads(request.body.decode("utf-8"))
         text = data["text"]
         voice_id = data["voice_id"]
         type = data["type"]
 
-        file_name = single_tts_driver.synthesis(
-            type=type, text=text, voice_id=voice_id)
+        file_name = single_tts_driver.synthesis(type=type, text=text, voice_id=voice_id)
         file_path = os.path.join("tmp", file_name)
 
         audio_file = BytesIO()
-        with open(file_path, 'rb') as file:
+        with open(file_path, "rb") as file:
             audio_file.write(file.read())
 
         delete_file(file_path)
@@ -38,8 +38,8 @@ def generate(request):
         audio_file.seek(0)
 
         # Create the response object.
-        response = HttpResponse(content_type='audio/mpeg')
-        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+        response = HttpResponse(content_type="audio/mpeg")
+        response["Content-Disposition"] = f'attachment; filename="{file_name}"'
         response.write(audio_file.getvalue())
         return response
     except Exception as e:
@@ -50,23 +50,28 @@ def generate(request):
 def delete_file(file_path):
     os.remove(file_path)
 
-@api_view(['POST'])
-def get_voices(request):
-    data = json.loads(request.body.decode('utf-8'))
-    type = data["type"]
-    return Response({"response": single_tts_driver.get_voices(type=type), "code": "200"})
 
-@api_view(['POST'])
+@api_view(["POST"])
+def get_voices(request):
+    data = json.loads(request.body.decode("utf-8"))
+    type = data["type"]
+    return Response(
+        {"response": single_tts_driver.get_voices(type=type), "code": "200"}
+    )
+
+
+@api_view(["POST"])
 def translation(request):
     """
     translation
     """
     try:
-        data = json.loads(request.body.decode('utf-8'))
+        data = json.loads(request.body.decode("utf-8"))
         text = data["text"]
         target_language = data["target_language"]
         target_result = translationClient.translation(
-            text=text, target_language=target_language)
+            text=text, target_language=target_language
+        )
         return Response({"response": target_result, "code": "200"})
     except Exception as e:
         logger.error(f"translation error: {e}")
